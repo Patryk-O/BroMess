0f5f5e49d67e2b3972c97b3061a8a2e95d7bb370
Patryk Ostafin <przeklety@me>
2021-10-29 14:10:35 +0200

    Add project files.

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BroMess.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user[0].ToString().ToUpper(), message+" Bro");
+        }
+    }
+}
---
+++ Server/Users/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BroMess.Server.Users
+{
+    public interface IUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public byte[] Avatar { get; set; }
+
+        public bool CreateUser();
+        public bool Login();
+        public IUser FindUser();
+    }
+}
